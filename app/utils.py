8731597ceecdd197df92cbdd357ee9da4804c167
Nina Menezes <nmenezes0@example.com>
2025-03-15 17:23:11 +0000

    add pages to homepage

--- app/utils.py
+++ app/utils.py
@@ -6,5 +6,6 @@ import streamlit as st
 
 @st.cache_data
 def get_pyladies_locations_data():
-    st.write(os.getcwd())
-    return pd.read_csv("app/data/pyladies_locations.csv")
+    df = pd.read_csv("app/data/pyladies_locations.csv")
+    return df
+
